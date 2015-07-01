8df66c2899f2a765190aec49c5e836c673598542
Simon Chiang <simon.a.chiang@gmail.com>
2015-06-30 21:52:35 -0600

    Add code to trigger an error to illustrate PATCH rollback issue

--- app/resources/phone_number_resource.rb
+++ app/resources/phone_number_resource.rb
@@ -3,4 +3,14 @@ class PhoneNumberResource < JSONAPI::Resource
   has_one :contact
 
   filter :contact
+
+  def name=(value)
+    if value == "boom"
+      model.name = "change to database that isn't rolled back"
+      model.save
+      raise "some error"
+    end
+
+    model.name = value
+  end
 end
